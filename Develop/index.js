be6ce4cc40f072a3a5ba53a2831e745e4390b57a
Mark <mark.munter1@gmail.com>
2022-04-03 17:56:46 -0500

    added first question, and require

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,24 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require("fs")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: "project",
+    message: "enter project name"
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions).then((answers) =>{
+console.log(answers)
+    })
+}
 
 // Function call to initialize app
 init();

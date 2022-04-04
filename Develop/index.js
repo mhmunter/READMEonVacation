4d76f8bdc63620f59f40f1e3e499539ccfc5b664
Mark <mark.munter1@gmail.com>
2022-04-04 09:32:22 -0500

    added Read.md generation

--- Develop/index.js
+++ Develop/index.js
@@ -1,22 +1,44 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer")
-const fs = require("fs")
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [{
     type: "input",
     name: "project",
-    message: "enter project name"
+    message: "Enter project name"
+},{
+type: "input",
+name: "github",
+message: "Enter github name"
+},{
+    type: "input",
+    name: "discription",
+    message: "Give a discription of your project" 
+},{
+    type: "list",
+    name: "license",
+    message: "What license would you like to use on your project",
+    choices: ["MIT", "GNU", "Apeche" ]
+
 }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+fs.writeFileSync(fileName, data)
+console.log(fileName)
+}
 // TODO: Create a function to initialize app
 function init() {
     inquirer
     .prompt(questions).then((answers) =>{
 console.log(answers)
+
+const template = generateMarkdown(answers)
+writeToFile("READ.md", template)
+
+console.log(generateMarkdown(answers))
     })
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,6 +13,10 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+ ${data.project}
+ ${data.github}
+ ${data.discription}
+ ${data.license}
 
 `;
 }

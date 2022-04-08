cc32aa4bc2953e7aaeecf08ff6eba49ea7d7a8a2
Mark <mark.munter1@gmail.com>
2022-04-08 10:54:34 -0500

    added readme vedio

--- Develop/READ.md
+++ Develop/READ.md
@@ -1,6 +1,7 @@
-# generator
- **mhmunters**
- **[Discription-]**abrbra cadbra
+# README.md
+ **mhmunter**
+ **[Discription-]**it creates a readme file using command line.
  MIT
- [e-mail:](github.com)
+ [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+ [e-mail:](mhmunter@github.io)
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
+if (license == "MIT") {
+  return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+}else{
+  return  '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+}
  
 
 }
@@ -20,6 +24,7 @@ function generateMarkdown(data) {
  **${data.github}**
  **[Discription-]**${data.discription}
  ${data.license}
+ ${renderLicenseBadge(data.license)}
  [e-mail:](${data.email})
 
 `;
